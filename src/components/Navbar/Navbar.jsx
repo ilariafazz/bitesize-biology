1e473eb27c1f4ecf3fbe164842dd8ea890f14d62
ilaria fazzolari <owner@ilarias-macbook-air.local>
2022-09-12 20:06:30 -0400

    mobile version supported

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,40 @@
 import React from "react";
-
+import { useState } from "react";
 import "./Navbar.css" ;
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import Logo from '../../assets/Logo.JPEG';
+
+const Menu = () => (
+    <>
+        <p><a className="link" href="#home">Home</a></p> 
+        <p><a className="link" href="#about-ilaria">About Ilaria</a></p> 
+        <p><a className="link" href="#about-dharini">About Dharini</a></p>
+        <p><a className="link" href="#our-youtube">Our Youtube</a></p> 
+    </>
+)
+
+
 const Navbar = () => {
+    const [toggleMenu, setToggleMenu] = useState(false);
     return (
-        <div className= "navbar">
-            <div className="navbar-links_container" >
-                <p><a className="link" href="#home">Home</a></p> 
-                <p><a className="link" href="#about-ilaria">About Ilaria</a></p> 
-                <p><a className="link" href="#about-dharini">About Dharini</a></p> 
-    
-                <p><a className="link" href="#our-youtube">Our Youtube</a></p> 
+        <div className= "portfolio__navbar">
+            <div className="portfolio__navbar-links" >
+                <div className="portfolio__navbar-links_container">
+                    <Menu />
+                </div>
+                <div className='portfolio__navbar-menu'>
+                    {toggleMenu 
+                        ? <RiCloseLine color="fff" size={27} onClick={()=> setToggleMenu(false)} />
+                        : <RiMenu3Line color="fff" size={27} onClick={()=> setToggleMenu(true)} />
+                    }
+                    {toggleMenu &&
+                        <div className='portfolio__navbar-menu_container scale-up-center'>
+                            <div className='portfolio__navbar-menu_container-links'>
+                                <Menu />
+                            </div>
+                        </div>
+                    }
+                </div>
             </div>
         </div>
   )
--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -11,20 +11,20 @@ const Header = () => {
             <div className="header">
                 <img src={noTextLogo} id="bunny" />
                 <div className="explanation-box">
-                    <h1 className="header-text">Defining the specifics of biology a digestible way, <br />Your first biology exposure starts today!</h1>
+                    <h1 className="header-text">Defining the specifics of biology in a digestible way, <br />Your first biology exposure starts today!</h1>
                     <h3 className="regular-text">Our mission is to expose kids to the biology all around them in an entertaining way. Through answering any questions we receive and providing engaging activities, we foster young curiosity with facts and understanding!</h3>
                 </div>
             </div>
             <div className="about-ilaria">
                 <div>
-                    <p>This summer I spent 24 days isolated in the wild, canoeing down the picturesque Missinaibi River in northern Ontario. I faced whatever nature threw at me daily, learning anything and everything I could about my surroundings.
+                    <p>Last summer I spent 24 days isolated in the wild, canoeing down the picturesque Missinaibi River in northern Ontario. I faced whatever nature threw at me daily, learning anything and everything I could about my surroundings.
                     This trip only further solidified my passion for biology and understanding the world around me. Ever since I was young I remember always being curious, always having questions and always genuinely wondering how things around me worked. 
                     <br />
-                    <br />Whether it was by spending countless days exploring the woods outside my Nonna’s farm, summers at sleepaway camp, immersed in the great outdoors, or even now in the lecture room of my grade eleven biology class, I love understanding how systems work together all throughout life. 
+                    <br />Whether it was by spending countless days exploring the woods outside my Nonna’s farm, summers at sleepaway camp, immersed in the great outdoors, or even in the lecture room of my grade eleven biology class, I love understanding how systems work together all throughout life. 
                     Naturally, meeting Dharini and starting BiteSize Biology allows me to share my passion for curiosity in the realm of biology to elementary students.</p>
                 </div>
-                <div id="pink-bunny">
-                    <img className="about-ilaria-bunny" src={backgroundlessBunny} />
+                <div>
+                    <img id="about-ilaria-bunny" src={backgroundlessBunny} />
                 </div>
             </div>
         </div>
